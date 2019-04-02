b00c8123f798aff1f603a87440ee6ab13e52507b
kelvin <89221143@qq.com>
2019-04-02 17:16:50 +0800

    init

---
+++ src/main/java/com/kelvin/javafxtest/MainApp.java
@@ -0,0 +1,25 @@
+package com.kelvin.javafxtest;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ */
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
+        Scene scene = new Scene(root, 600, 500);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("application.css").toExternalForm());
+        primaryStage.setTitle("Simple JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/com/kelvin/javafxtest/MainController.java
@@ -0,0 +1,16 @@
+package com.kelvin.javafxtest;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+/**
+ * Created by tanzhenyu on 2017/6/14.
+ */
+public class MainController {
+    @FXML
+    public void handlerBtnClick(ActionEvent event) {
+        Button btnSource = (Button) event.getSource();
+        btnSource.setText("I am clicked!");
+    }
+}
